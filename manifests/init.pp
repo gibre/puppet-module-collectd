39b1d3231d930b2433891cb9c5fdea096f5a4097
Gil Brechbuhler <Gil.Brechbuhler@epfl.ch>
2015-07-21 18:20:23 +0200

    move install case

--- manifests/init.pp
+++ manifests/init.pp
@@ -22,8 +22,38 @@ class collectd(
   validate_bool($purge_config, $fqdnlookup)
   validate_array($include, $typesdb)
 
-  class { 'collectd::install' :
-    before   => File['collectd.conf', 'collectd.d'],
+  case $::collectd::install_method {
+    'package' : {
+      notice ("package installation")
+      package { $collectd::package_name:
+        ensure   => $collectd::version,
+        name     => $collectd::package_name,
+        provider => $collectd::params::provider,
+        before   => File['collectd.conf', 'collectd.d'],
+      }
+    }
+    'script' : {
+      notice ("installing via script")
+      file {
+      'install_collectd':
+        ensure => 'file',
+        source => $collectd::install_script_path,
+        path   => '/usr/local/bin/install_collectd.sh',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0744',
+        notify => Exec['run_collectd_install'],
+      } ->
+      exec {
+        'run_collectd_install':
+         command     => '/usr/local/bin/install_collectd.sh',
+         refreshonly => true,
+         before   => File['collectd.conf', 'collectd.d'],
+      }
+    }
+    default: {
+      fail("Value for collectd::manage_install not supported")
+    }
   }
 
 
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,33 +1,3 @@
 class collectd::install {
-  case $::collectd::install_method {
-    'package' : {
-      notice ("package installation")
-      package { $collectd::package_name:
-        ensure   => $collectd::version,
-        name     => $collectd::package_name,
-        provider => $collectd::params::provider,
-      }
-    }
-    'script' : {
-      notice ("installing via script")
-      file {
-      'install_collectd':
-        ensure => 'file',
-        source => $collectd::install_script_path,
-        path   => '/usr/local/bin/install_collectd.sh',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0744',
-        notify => Exec['run_collectd_install'],
-      } ->
-      exec {
-        'run_collectd_install':
-         command     => '/usr/local/bin/install_collectd.sh',
-         refreshonly => true,
-      }
-    }
-    default: {
-      fail("Value for collectd::manage_install not supported")
-    }
-  }
+  
 }
